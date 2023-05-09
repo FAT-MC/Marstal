e916b57a71962c0fee59f33638c0e5f1312e02df
Marko Ma <markomadev@gmail.com>
2023-05-09 22:51:14 +0800

    re-authenticate socket connection with new token if old token expires; reconnect socket on server disconnect due to token expiration while being connected;

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, createContext } from 'react';
 import io from 'socket.io-client';
-import { retrieveToken } from "../utils/storageManager"
+import { getAccessToken, refreshAccessToken } from "../service/tokenService";
 
 const SocketContext = createContext();
 
@@ -9,7 +9,7 @@ function SocketProvider({ children }) {
   const [audioResponse, setAudioResponse] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(process.env.REACT_APP_SERVER_URL, { auth: { token: retrieveToken() } });
+    const newSocket = io(process.env.REACT_APP_SERVER_URL, { auth: { token: getAccessToken() } });
     setSocket(newSocket);
 
     return () => {
@@ -26,12 +26,21 @@ function SocketProvider({ children }) {
       console.log(`Socket ${socket.id} Connected to server`);
     });
 
-    socket.on('disconnect', () => {
-      console.log(`Socket ${socket.id} Disconnected from server`);
+    socket.on('disconnect', (reason) => {
+      console.log(`Socket Disconnected from server`);
+      if (reason === "io server disconnect") {
+        // the disconnection was initiated by the server, you need to reconnect manually
+        socket.connect();
+      }
     });
 
-    socket.on("connect_error", (err) => {
+    socket.on("connect_error", async (err) => {
       console.error(err);
+      if (err.message === "Unauthorized") {
+        const newAccessToken = await refreshAccessToken();
+        socket.auth.token = newAccessToken;
+        socket.connect();
+      }
     });
 
     socket.on('response', (aiAudioResponse) => {
